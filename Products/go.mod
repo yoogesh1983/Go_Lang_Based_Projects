4a9158a0114f120f134b7e314f5a806b18e70886
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-10-17 22:03:31 -0400

    Validation using go validation API

--- Products/go.mod
+++ Products/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/fatih/color v1.9.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-hclog v0.14.1 // indirect
--- Products/src/handlers/products.go
+++ Products/src/handlers/products.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
-	data "Products/src/data"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+
+	data "Products/src/data"
 
 	"github.com/gorilla/mux"
 )
@@ -74,6 +76,18 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 		if err != nil {
 			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
 			return
 		}
 
